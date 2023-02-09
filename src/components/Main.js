05dd56d2a8c40eb68307889a0098f5d007362cf2
Dario Bristot <dariiob19@gmail.com>
2023-02-08 21:29:29 -0300

    agregado de greeting

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './estilos/main.css'
+
+
+
+export const  ItemListContainer = () => {
+
+    const greeting = 'Equipa tu casa con los mejores precios'
+
+    return (
+        <>
+        <div className='greetingContainer'>
+            <h2>{greeting}</h2>
+        </div>
+        </>
+
+    )
+
+}
